0d6d01a7e71f5a0c3fe769d8a11f00479a7c068a
yeonseong <yeonseong.dev@gmail.com>
2025-01-19 19:16:35 +0900

    fix: font korean name

--- src/lib/fonts.js
+++ src/lib/fonts.js
@@ -13,7 +13,7 @@ import localFont from 'next/font/local'
 export const pretendard = localFont({
   src: [
     {
-      path: '../../public/fonts/Pretendard-Regular.ttf',
+      path: '../../public/fonts/프리텐다드.ttf',
       weight: '400',
       style: 'normal',
     }
@@ -23,238 +23,238 @@ export const pretendard = localFont({
 })
 
 export const watermelon = localFont({
-  src: '../../public/fonts/116watermelon.ttf',
+  src: '../../public/fonts/116수박화체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const bagelfatone = localFont({
-  src: '../../public/fonts/BagelFatOne-Regular.ttf',
+  src: '../../public/fonts/베이글팻.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const banggraeMelon = localFont({
-  src: '../../public/fonts/BinggraeMelona.ttf',
+  src: '../../public/fonts/빙그레메로나체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const bmdohyeon = localFont({
-  src: '../../public/fonts/BMDOHYEON_ttf.ttf',
+  src: '../../public/fonts/도현체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const bmeuljiro10yearslater = localFont({
-  src: '../../public/fonts/BMEuljiro10YearsLater.ttf',
+  src: '../../public/fonts/을지로10년후체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const bmeuljirooraeorae = localFont({
-  src: '../../public/fonts/BMEuljirooraeorae.ttf',
+  src: '../../public/fonts/을지로오래오래체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const bmhannaair = localFont({
-  src: '../../public/fonts/BMHANNAAir_ttf.ttf',
+  src: '../../public/fonts/한나체Air.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const cafe24ClassicType = localFont({
-  src: '../../public/fonts/Cafe24ClassicType-v1.1.ttf',
+  src: '../../public/fonts/카페24클래식타입.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const cafe24Ohsquare = localFont({
-  src: '../../public/fonts/Cafe24Ohsquare-v2.0.ttf',
+  src: '../../public/fonts/카페24아네모네.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const cafe24OneprettyNight = localFont({
-  src: '../../public/fonts/Cafe24Oneprettynight-v2.0.ttf',
+  src: '../../public/fonts/카페24고운밤.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const cafe24SsurroundAir = localFont({
-  src: '../../public/fonts/Cafe24SsurroundAir-v1.1.ttf',
+  src: '../../public/fonts/카페24써라운드에어.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const chab = localFont({
-  src: '../../public/fonts/chab.ttf',
+  src: '../../public/fonts/리아체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const changwonDangamRound = localFont({
-  src: '../../public/fonts/ChangwonDangamRound.ttf',
+  src: '../../public/fonts/창원단감아삭체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const chosunGu = localFont({
-  src: '../../public/fonts/ChosunGu.ttf',
+  src: '../../public/fonts/조선굴림체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const climateCrisisKR2010 = localFont({
-  src: '../../public/fonts/ClimateCrisisKR-2010.ttf',
+  src: '../../public/fonts/기후위기-한글.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const d2Coding = localFont({
-  src: '../../public/fonts/D2Coding-ver1.3.2-20180524.ttf',
+  src: '../../public/fonts/D2Coding.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const ddag = localFont({
-  src: '../../public/fonts/ddag.ttf',
+  src: '../../public/fonts/롯데리아딱붙어체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const dnfforgedblade = localFont({
-  src: '../../public/fonts/DNFForgedBlade-Medium.ttf',
+  src: '../../public/fonts/던파연단된칼날.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const dosGothic = localFont({
-  src: '../../public/fonts/DOSGothic.ttf',
+  src: '../../public/fonts/도스고딕.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const dungGeunMo = localFont({
-  src: '../../public/fonts/DungGeunMo.ttf',
+  src: '../../public/fonts/둥근모꼴+Fixedsys.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const efCucumbersalad = localFont({
-  src: '../../public/fonts/EF_Cucumbersalad(ttf).ttf',
+  src: '../../public/fonts/오이냉체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const efJejudoldam = localFont({
-  src: '../../public/fonts/EF_Jejudoldam(ttf).ttf',
+  src: '../../public/fonts/제주돌담체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const efMacho = localFont({
-  src: '../../public/fonts/EF_Macho(ttf).ttf',
+  src: '../../public/fonts/마초체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const elandNice = localFont({
-  src: '../../public/fonts/ELAND_나이스_M.ttf',
+  src: '../../public/fonts/이랜드나이스체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const elandChoice = localFont({
-  src: '../../public/fonts/ELAND_초이스_M.ttf',
+  src: '../../public/fonts/이랜드초이스체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const eliceDigitalCoding = localFont({
-  src: '../../public/fonts/EliceDigitalCodingverH_Regular.ttf',
+  src: '../../public/fonts/엘리스디지털코딩체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const esamanru = localFont({
-  src: '../../public/fonts/esamanru_Medium.ttf',
+  src: '../../public/fonts/이사만루.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const establishRetrosans = localFont({
-  src: '../../public/fonts/establish_Retrosans.ttf',
+  src: '../../public/fonts/설립체유건욱.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const galmuri11 = localFont({
-  src: '../../public/fonts/Galmuri11.ttf',
+  src: '../../public/fonts/갈무리11.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const giant = localFont({
-  src: '../../public/fonts/Giants-Regular.ttf',
+  src: '../../public/fonts/자이언츠체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const gmarketSansTTF = localFont({
-  src: '../../public/fonts/GmarketSansTTFMedium.ttf',
+  src: '../../public/fonts/G마켓산스.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const goormSansRegular = localFont({
-  src: '../../public/fonts/goorm-sans-regular.ttf',
+  src: '../../public/fonts/구름산스.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const gumiIndustry = localFont({
-  src: '../../public/fonts/Gumi-Industry.ttf',
+  src: '../../public/fonts/구미첨단산업체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const hahmlet = localFont({
-  src: '../../public/fonts/Hahmlet-Medium.ttf',
+  src: '../../public/fonts/함렛.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
@@ -310,35 +310,35 @@ export const gaegu = Gaegu({
 })
 
 export const hakgyoansimAllimjang = localFont({
-  src: '../../public/fonts/Hakgyoansim-Allimjang-TTF-R.ttf',
+  src: '../../public/fonts/학교안심알림장.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const hakgyoansimDunggeunmiso = localFont({
-  src: '../../public/fonts/Hakgyoansim-Dunggeunmiso-TTF-R.ttf',
+  src: '../../public/fonts/학교안심둥근미소.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const hakgyoansimPuzzle = localFont({
-  src: '../../public/fonts/Hakgyoansim-Puzzle-TTF-Black.ttf',
+  src: '../../public/fonts/학교안심퍼즐.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const hancomMalangMalang = localFont({
-  src: '../../public/fonts/HancomMalangMalang-Bold.ttf',
+  src: '../../public/fonts/한컴말랑말랑.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const heirOfLight = localFont({
-  src: '../../public/fonts/HeirofLightRegular.ttf',
+  src: '../../public/fonts/빛의계승자체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
@@ -352,336 +352,336 @@ export const hssBomBaram = localFont({
 })
 
 export const hssSantokki = localFont({
-  src: '../../public/fonts/HSSantokki-Regular.ttf',
+  src: '../../public/fonts/HS산토끼체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const hssSantokki2 = localFont({
-  src: '../../public/fonts/HSSanTokki2.0(2024).ttf',
+  src: '../../public/fonts/HS산토끼체2.0.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const intelOneMono = localFont({
-  src: '../../public/fonts/IntelOneMono-Medium.ttf',
+  src: '../../public/fonts/IntelOneMono.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const jalnan2 = localFont({
-  src: '../../public/fonts/Jalnan2TTF.ttf',
+  src: '../../public/fonts/여기어때잘난체2.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const koPubBatang = localFont({
-  src: '../../public/fonts/KoPub-Batang-Medium.ttf',
+  src: '../../public/fonts/KoPub바탕.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const lineSeedKR = localFont({
-  src: '../../public/fonts/LINESeedKR-Rg.ttf',
+  src: '../../public/fonts/LINESeed.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const mabinogiClassic = localFont({
-  src: '../../public/fonts/Mabinogi_Classic_TTF.ttf',
+  src: '../../public/fonts/마비옛체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const mangoDdobak = localFont({
-  src: '../../public/fonts/MangoDdobak-R(ttf).ttf',
+  src: '../../public/fonts/망고보드또박체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const maplestory = localFont({
-  src: '../../public/fonts/Maplestory-Light.ttf',
+  src: '../../public/fonts/넥슨메이플스토리.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const moneygraphy = localFont({
-  src: '../../public/fonts/Moneygraphy-Pixel.ttf',
+  src: '../../public/fonts/머니그라피-픽셀.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const moveSans = localFont({
-  src: '../../public/fonts/MoveSans-Medium.ttf',
+  src: '../../public/fonts/MoveSans.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const mungyeongGamhong = localFont({
-  src: '../../public/fonts/Mungyeong-Gamhong-Apple.ttf',
+  src: '../../public/fonts/문경감홍사과체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const nexonLv1Gothic = localFont({
-  src: '../../public/fonts/NEXONLv1GothicRegular.ttf',
+  src: '../../public/fonts/넥슨Lv.1고딕.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const nanumGothic = localFont({
-  src: '../../public/fonts/NanumGothic.ttf',
+  src: '../../public/fonts/나눔고딕.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const nanumSquare = localFont({
-  src: '../../public/fonts/NanumSquareR.ttf',
+  src: '../../public/fonts/나눔스퀘어.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const nanumSquareRound = localFont({
-  src: '../../public/fonts/NanumSquareRoundR.ttf',
+  src: '../../public/fonts/나눔스퀘어라운드.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const noonnuBasicGothic = localFont({
-  src: '../../public/fonts/NoonnuBasicGothicRegular.ttf',
+  src: '../../public/fonts/눈누기초고딕.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const notoSansKR = localFont({
-  src: '../../public/fonts/NotoSansKR-Medium.ttf',
+  src: '../../public/fonts/본고딕.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const okGung = localFont({
-  src: '../../public/fonts/OK-GUNG.ttf',
+  src: '../../public/fonts/읏맨-궁서체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const okmanFont = localFont({
-  src: '../../public/fonts/OKMAN-FONT.ttf',
+  src: '../../public/fonts/읏맨-엉뚱체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const oneMobileTitle = localFont({
-  src: '../../public/fonts/ONE-Mobile-Title.ttf',
+  src: '../../public/fonts/원스토어모바일고딕제목체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const orbit = localFont({
-  src: '../../public/fonts/Orbit-Regular.ttf',
+  src: '../../public/fonts/오르빗.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const paperlogy = localFont({
-  src: '../../public/fonts/Paperlogy-4Regular.ttf',
+  src: '../../public/fonts/페이퍼로지.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const puradakGentleGothic = localFont({
-  src: '../../public/fonts/Puradak-Gentle-Gothic.ttf',
+  src: '../../public/fonts/푸라닭젠틀고딕.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const pyeongChangPeace = localFont({
-  src: '../../public/fonts/PyeongChangPeace-Light.ttf',
+  src: '../../public/fonts/평창체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const roehoeChan = localFont({
-  src: '../../public/fonts/ROEHOE-CHAN.ttf',
+  src: '../../public/fonts/노회찬체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const rokafSlabSerif = localFont({
-  src: '../../public/fonts/ROKAF-Slab-Serif-Medium.ttf',
+  src: '../../public/fonts/바른공군체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const riaSans = localFont({
-  src: '../../public/fonts/RiaSans-ExtraBold.ttf',
+  src: '../../public/fonts/리아체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const sdSamliphopangche = localFont({
-  src: '../../public/fonts/SDSamliphopangcheTTFBasic.ttf',
+  src: '../../public/fonts/삼립호빵체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const sdSamliphopangcheOutline = localFont({
-  src: '../../public/fonts/SDSamliphopangcheTTFOutline.ttf',
+  src: '../../public/fonts/삼립호빵체outline.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const sebangGothic = localFont({
-  src: '../../public/fonts/SEBANG-Gothic.ttf',
+  src: '../../public/fonts/세방고딕체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const sfSsaraknun = localFont({
-  src: '../../public/fonts/SF싸락눈.ttf',
+  src: '../../public/fonts/싸락눈체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const smuSnowflake = localFont({
-  src: '../../public/fonts/SMUSnowflake-Regular.ttf',
+  src: '../../public/fonts/숙명눈꽃체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const sogangUniversity = localFont({
-  src: '../../public/fonts/SOGANG_UNIVERSITY_for_windows.ttf',
+  src: '../../public/fonts/서강체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const sam3KRFont = localFont({
-  src: '../../public/fonts/Sam3KRFont.ttf',
+  src: '../../public/fonts/삼국지3글꼴.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const tdtdTadakTadak = localFont({
-  src: '../../public/fonts/TDTDTadakTadak.ttf',
+  src: '../../public/fonts/타닥타닥체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const tenada = localFont({
-  src: '../../public/fonts/Tenada.ttf',
+  src: '../../public/fonts/태나다체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const theJamsil = localFont({
-  src: '../../public/fonts/The-Jamsil-3-Regular.ttf',
+  src: '../../public/fonts/더잠실체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const tmoneyRoundWind = localFont({
-  src: '../../public/fonts/TmoneyRoundWindRegular.ttf',
+  src: '../../public/fonts/티머니둥근바람.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const vitroCore = localFont({
-  src: '../../public/fonts/VITRO-CORE-TTF.ttf',
+  src: '../../public/fonts/비트로코어체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const vitroInspire = localFont({
-  src: '../../public/fonts/VITRO-INSPIRE-TTF.ttf',
+  src: '../../public/fonts/비트로인스파이어체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const vitroPride = localFont({
-  src: '../../public/fonts/VITRO-PRIDE-TTF.ttf',
+  src: '../../public/fonts/비트로프라이드체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const wantedSans = localFont({
-  src: '../../public/fonts/WantedSans-Regular.ttf',
+  src: '../../public/fonts/WantedSans.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const yOnepick = localFont({
-  src: '../../public/fonts/YOnepick-Regular.ttf',
+  src: '../../public/fonts/Y최애체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const yUniverse = localFont({
-  src: '../../public/fonts/YUniverse-L.ttf',
+  src: '../../public/fonts/Y유니버스체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const ycomputer = localFont({
-  src: '../../public/fonts/Ycomputer.ttf',
+  src: '../../public/fonts/Y콤퓨타체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const yeongdo = localFont({
-  src: '../../public/fonts/Yeongdo-Regular.ttf',
+  src: '../../public/fonts/영도체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const twaySky = localFont({
-  src: '../../public/fonts/tway_sky.ttf',
+  src: '../../public/fonts/티웨이날다체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
@@ -695,21 +695,21 @@ export const binpol2020 = localFont({
 })
 
 export const miwon = localFont({
-  src: '../../public/fonts/미원Miwon.ttf',
+  src: '../../public/fonts/미원체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const ongleafConcon = localFont({
-  src: '../../public/fonts/온글잎-콘콘체.ttf',
+  src: '../../public/fonts/온글잎콘콘체.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
 })
 
 export const gangwonEduAll = localFont({
-  src: '../../public/fonts/강원교육모두-Light.ttf',
+  src: '../../public/fonts/강원교육모두.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
@@ -723,7 +723,7 @@ export const gangwonEduPower = localFont({
 })
 
 export const gyeonggiCheonnyeonBatang = localFont({
-  src: '../../public/fonts/경기천년바탕_Regular.ttf',
+  src: '../../public/fonts/경기천년바탕.ttf',
   weight: '400',
   style: 'normal',
   display: 'swap',
