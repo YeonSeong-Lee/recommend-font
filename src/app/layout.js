724b6087fc00f62d5f431b659b5673c5c5bbd655
yeonseong <yeonseong.dev@gmail.com>
2025-01-20 21:54:31 +0900

    chore: change head, meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,12 +7,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: '폰트 추천 서비스',
-  description: '당신에게 딱 맞는 폰트를 찾아보세요',
+  description: '가장 비슷한 폰트를 찾아드립니다.',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko" suppressHydrationWarning>
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-7275281216554032" />
+      </head>
       <body className={`${inter.className} flex flex-col min-h-screen bg-white text-gray-900 transition-colors duration-200
         dark:bg-gray-950 dark:text-gray-100`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
